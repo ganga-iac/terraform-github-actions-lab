869deb3d9a660ed1dba46ca32159b4aaac624808
sivareddy8143 <thotasivareddy1998@gmail.com>
2026-08-14 22:36:44 +0530

    Add the terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet" {
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ami" {
+  description = "Amazon Linux 2023 AMI"
+}
