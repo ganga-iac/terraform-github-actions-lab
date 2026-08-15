ab371c9835b0b72720c84f62894dc96301f351ea
sivareddy8143 <thotasivareddy1998@gmail.com>
2026-08-15 17:50:29 +0530

    Update the terraform yaml file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,13 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet" {
-              default = "10.0.1.0/24"
+  default = "10.0.1.0/24"
 }
 
 variable "instance_type" {
-             default = "t3.micro"
+  default = "t3.micro"
 }
 
 variable "ami" {
-        description = "Amazon Linux 2023 AMI"
+  description = "Amazon Linux 2023 AMI"
 }
